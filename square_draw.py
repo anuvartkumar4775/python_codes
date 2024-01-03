36593ba8e81f0e550ea983a3b2bdfa68e3400ba3
anuvart kumar <80092802+anuvartkumar4775@users.noreply.github.com>
2024-01-03 19:01:40 +0530

    Add files via upload

---
+++ square_draw.py
@@ -0,0 +1,17 @@
+import turtle
+
+T = turtle.Turtle()
+ 
+side = int(input("enter the length of the side of the square: "))
+
+T.forward(side)
+T.right(90)
+
+T.forward(side)
+T.right(90)
+ 
+T.forward(side)
+T.right(90) 
+ 
+T.forward(side)
+T.right(90)
